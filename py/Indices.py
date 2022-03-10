792c02eec82be6827319b2599d575313db0361e8
Vinicius173203 <vinicius151571@gmail.com>
2022-03-10 19:17:52 -0300

    commit inicial

---
+++ py/Indices.py
@@ -0,0 +1,6 @@
+print('Olá vamos te ajudar no cálculo de seus indices finaceiros')
+print('Você sabe')
+AT = int(input("Primeiramente digite o seu ativo total: $"))
+PT = int(input("Agora digite o seu passivo total: $"))
+RC = AT/PT
+print("Para cada um real de divída a empresa na qual foram fornecidas os dados tem $",RC, "para saldar suas dividas.")
